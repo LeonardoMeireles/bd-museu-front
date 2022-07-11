fb437d9340d098a4c8967a127a9d5c70182183d7
LeonardoMeireles <leo.meireles01@gmail.com>
2022-07-11 00:27:40 -0300

    feat: wow that's a big commit 😳👉👈

---
+++ src/routes.js
@@ -0,0 +1,94 @@
+/*!
+
+=========================================================
+* Black All React v1.2.1
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/black-dashboard-react
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/black-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import All from "views/All.js";
+import Collections from "views/Collections.js";
+import Icons from "views/Icons.js";
+import Map from "views/Map.js";
+import Notifications from "views/Notifications.js";
+import Rtl from "views/Rtl.js";
+import TableList from "views/TableList.js";
+import Typography from "views/Typography.js";
+import UserProfile from "views/UserProfile.js";
+
+var routes = [
+  {
+    path: "/todos",
+    name: "Todos",
+    rtlName: "لوحة القيادة",
+    icon: "tim-icons icon-palette",
+    component: All,
+    layout: "/admin"
+  },
+  {
+    path: "/coleções",
+    name: "Coleções",
+    rtlName: "الرموز",
+    icon: "tim-icons icon-bank",
+    component: Collections,
+    layout: "/admin"
+  },
+  // {
+  //   path: "/map",
+  //   name: "Map",
+  //   rtlName: "خرائط",
+  //   icon: "tim-icons icon-pin",
+  //   component: Map,
+  //   layout: "/admin"
+  // },
+  // {
+  //   path: "/notifications",
+  //   name: "Notifications",
+  //   rtlName: "إخطارات",
+  //   icon: "tim-icons icon-bell-55",
+  //   component: Notifications,
+  //   layout: "/admin"
+  // },
+  // {
+  //   path: "/user-profile",
+  //   name: "User Profile",
+  //   rtlName: "ملف تعريفي للمستخدم",
+  //   icon: "tim-icons icon-single-02",
+  //   component: UserProfile,
+  //   layout: "/admin"
+  // },
+  // {
+  //   path: "/tables",
+  //   name: "Table List",
+  //   rtlName: "قائمة الجدول",
+  //   icon: "tim-icons icon-chart-bar-32",
+  //   component: TableList,
+  //   layout: "/admin"
+  // },
+  // {
+  //   path: "/typography",
+  //   name: "Typography",
+  //   rtlName: "طباعة",
+  //   icon: "tim-icons icon-align-center",
+  //   component: Typography,
+  //   layout: "/admin"
+  // },
+  // {
+  //   path: "/rtl-support",
+  //   name: "RTL Support",
+  //   rtlName: "ار تي ال",
+  //   icon: "tim-icons icon-world",
+  //   component: Rtl,
+  //   layout: "/rtl"
+  // }
+];
+export default routes;
